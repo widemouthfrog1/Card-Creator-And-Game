14ea81c93c5e5ce71d8ce5cbab9ce4b91b5ed080
Karl Bennett <widemouthfrog1@gmail.com>
2020-04-20 13:34:40 +1200

    Initial Commit

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Container : Item
+{
+    public Vector2 size = new Vector2(0,0);
+    public List<Item> items = new List<Item>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Generate Mesh")]
+    public virtual void GenerateMesh()
+    {
+
+    }
+}
